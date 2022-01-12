e3249c6a9850226e3d93598cedec2b3934dca5a7
bokijonovm <muhsinjonboqijonov2002@gmail.com>
2022-01-12 14:05:43 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -7,12 +7,17 @@ import { Container, Row, Col } from 'react-bootstrap'
 function App() {
   return (
     <div className="App">
+        <div className='neon-wrapper py-4 pl-3'>
+          <h1 className='neon-text'>SpiderMan</h1> 
+          <h1 className='neon-text mb-0 pb-1'>HomeComing</h1>
+        </div>
+      
      
         <Row className='main-row' style={{ flexWrap: "nowrap" }}>
-          <Col md={6} className='pr-0' style={{ paddingRight: "0px" }}>
+          <Col md={6} className='p-0 pl-3 pt-5' style={{ paddingRight: "0px" }}>
             <IronMan />
           </Col>
-          <Col md={6} className='pl-0'  >
+          <Col md={6} className='p-0 pl-3 pt-5'  >
             <Spider />
           </Col>
         </Row>
